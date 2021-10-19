6adfdf40b46467da3bb7d56bfafa972d1c4e555d
Jay Cruz <49811999+CoderJay06@users.noreply.github.com>
2021-10-19 12:28:04 -0400

    initial commit

--- src/components/display.js
+++ src/components/display.js
@@ -11,7 +11,7 @@ function Display({ operand1, operand2, operator, equalSign, result }) {
       className="display"
       value={`${operand1 ? operand1 : "0"} ${operator ? operator : ""} ${
         operand2 ? operand2 : ""
-      } ${equalSign ? equalSign : ""} ${result ? result : ""}`}
+      } ${equalSign ? equalSign : ""} ${result || result === 0 ? result : ""}`}
       disabled
     />
   );
